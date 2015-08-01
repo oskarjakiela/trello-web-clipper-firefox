ce23b7d32d12f42fc7661772f3c35d48e1888804
Oskar S. Jakieła <oskar@jakiela.pl>
2015-08-01 22:48:55 +0200

    Rename addon to Trello Web Clipper

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-    .module('oj.trelloClipper')
+    .module('oj.trelloWebClipper')
     .config(config);
 
   /** @ngInject */
--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -4,7 +4,7 @@
   'use strict';
 
   angular
-    .module('oj.trelloClipper')
+    .module('oj.trelloWebClipper')
     .constant('self', self)
     .constant('Trello', Trello);
 
