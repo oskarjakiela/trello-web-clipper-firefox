044ebf991c79ac119dff10fb9f71310fc8256af4
Oskar S. Jakieła <oskar@jakiela.pl>
2015-08-02 14:10:45 +0200

    Fix storing token, board id and list id

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,9 +6,14 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log, $addon, Trello) {
+  function runBlock($log, $addon, $state, Trello) {
     $addon.prefs.onChange(function() {
       Trello.setKey($addon.prefs.key);
+    });
+
+    $addon.storage.onChange(function() {
+      Trello.setToken($addon.storage.token);
+      $state.go('main', {}, { reload: true });
     });
   }
 
