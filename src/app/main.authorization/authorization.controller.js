4108d7905c9cbc3a5d9eeb8b1b290fba15be983a
Oskar S. Jakieła <oskar@jakiela.pl>
2015-08-03 20:25:30 +0200

    Add option "Keep log in for..."

--- src/app/main.authorization/authorization.controller.js
+++ src/app/main.authorization/authorization.controller.js
@@ -3,19 +3,32 @@
 
   angular
     .module('oj.trelloWebClipper')
+    .value('expirations', {
+      '1hour': 'for 1 hour',
+      '1day': 'for 1 day',
+      '30days': 'for 30 days',
+      'never': 'forever'
+    })
     .controller('AuthorizationController', AuthorizationController);
 
   /** @ngInject */
-  function AuthorizationController($log, $addon, $state, Trello) {
+  function AuthorizationController($log, $addon, expirations, $state, Trello) {
     var vm = this;
 
+    vm.expiration = '30days';
+    vm.expirations = expirations;
     vm.logIn = logIn;
 
     function logIn() {
       Trello.authorize({
         name: $addon.pkg.title,
         type: 'popup',
-        scope: { read: true, write: true, account: false },
+        scope: {
+          read: true,
+          write: true,
+          account: false
+        },
+        expiration: vm.expiration,
         success: function() {
           $addon.storage.save('token', Trello.token());
           $state.go('main', {}, { reload: true });
